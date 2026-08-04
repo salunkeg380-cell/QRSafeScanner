b51453644c9c53cdfe88a04113424c6af0c5a0d8
Salunkeg380-cell <salunkeg380@gmail.com>
2026-08-04 13:22:45 +0530

    Detect suspicious keywords and IP-based URLs

--- src/URLAnalyzer.java
+++ src/URLAnalyzer.java
@@ -2,15 +2,15 @@ public class URLAnalyzer {
 
     public boolean isPhishing(String url) {
 
-        // Convert URL to lowercase
+       
         url = url.toLowerCase();
 
-        // Rule 1: URL contains an IP address
+       
         if(url.matches(".*\\d+\\.\\d+\\.\\d+\\.\\d+.*")) {
              return true;
         }
 
-        // Rule 2: Suspicious keywords
+       
         String[] suspiciousWords = {
                 "login",
                 "verify",
@@ -32,12 +32,12 @@ public class URLAnalyzer {
 
         }
 
-        // Rule 3: URL is too long
+        
         if(url.length() > 80) {
             return true;
         }
 
-        // Otherwise Safe
+        
         return false;
     }
 
